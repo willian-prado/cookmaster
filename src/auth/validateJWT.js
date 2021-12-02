5c454bb781f29fb703e23873951c434b89cb0d18
Willian Prado <willian_prado@hotmail.com>
2021-12-01 23:51:42 -0300

    Sets up an auth middleware

---
+++ src/auth/validateJWT.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const rescue = require('express-rescue');
+const userModel = require('../model/users');
+
+const secret = 'minhasecretkey';
+
+const err = {
+  isJoi: false,
+  type: 'UNAUTHORIZED', 
+  code: 401,
+  message: 'jwt malformed',
+};
+
+module.exports = rescue(async (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) return next({ err });
+
+  const decoded = jwt.verify(token, secret);
+
+  const user = await userModel.getByEmail(decoded.email);
+
+  if (!user) return next({ err });
+
+  req.user = user;
+  next();
+});
